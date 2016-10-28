8331e3f9c99ba1a65db938a7bcbeb9c2abcda00b
xixilide <754396775@qq.com>
2016-10-28 10:33:56 +0800

    i

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-import  'bootstrap/dist/css/bootstrap.min.css';
-import Routers from './routers';
-import './main.css';
-import 'highlight.js/styles/github.css';
-import './post.css';
-render(<Routers />,document.getElementById('root'));
